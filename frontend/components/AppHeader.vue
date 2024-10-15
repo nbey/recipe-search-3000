72488cda8257f2feaadbcab2ed78c5ec6bb9d6c9
Nafis Bey <nafis@jarv.us>
2024-10-15 16:30:59 -0400

    fix: app header link
    - ensure that page gets reloaded when clicked

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -1,14 +1,14 @@
 <template>
   <header class="flex w-full items-center justify-center">
-    <ULink
-      :to="`/recipes`"
+    <a
+      href="/recipes"
       class="no-underline"
     >
       <div class="flex m-4">
         <BookOpenIcon class="w-8 h-8 mr-3 text-green-300" />
         <h1 class="text-green-400 text-2xl font-bold">Recipe Search 3000</h1>
       </div>
-    </ULink>
+    </a>
   </header>
 </template>
 
